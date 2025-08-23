e1771897b82668a5a41714f6c3503765e146ac36
JCadena7 <lorenzofastidio176@gmail.com>
2025-08-22 20:21:05 -0500

    Ms Categoria conect with Gateway Ok

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+import { envs } from './config';
+
+async function bootstrap() {
+  const logger = new Logger('CategoriasBoostrap');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        // host: 'envs.host',
+        port: envs.port,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+  logger.log(`🚀 categorias microdervice running on port ${envs.port}`);
+}
+bootstrap();
